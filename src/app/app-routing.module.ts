b5d919ee5ce9965e021b022f2c0dcaaca6206eff
Diana Villarreal <dianacarolinav97@gmail.com>
2023-06-07 17:22:27 -0500

    feat: routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthComponent } from './auth/auth/auth.component';
+import { DashboardCreateComponent } from './dashboard/dashboard-create/dashboard-create.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', redirectTo:'/login', pathMatch:'full'},
+  {path:'login', component:AuthComponent},
+  {path:'dashboard', component:DashboardCreateComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
